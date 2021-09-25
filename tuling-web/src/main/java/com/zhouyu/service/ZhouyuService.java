cbd276a24bb6f3cab646d85ac28bb1d772ff6e18
linmeili <983781237@qq.com>
2021-09-25 16:29:15 +0800

    导入spring5.3.10

---
+++ tuling-web/src/main/java/com/zhouyu/service/ZhouyuService.java
@@ -0,0 +1,14 @@
+package com.zhouyu.service;
+
+import com.zhouyu.controller.ZhouyuController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ZhouyuService {
+
+
+
+
+}
+
---
+++ tuling/src/main/java/com/zhouyu/Test.java
@@ -0,0 +1,30 @@
+package com.zhouyu;
+
+import com.zhouyu.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.ClassMetadata;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Scheduled()
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+
+//
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		UserService userService = (UserService) context.getBean("userService");
+
+		userService.test();
+
+	}
+}
